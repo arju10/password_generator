87614ff74cd3176f9fd6ebe8a77da96f64a251bd
arju10 <arjuarju71@gmail.com>
2021-01-21 17:57:36 +0600

    commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import string  # String is imported from "string module python docs"
+import random
+
+s1  = string.ascii_lowercase
+s2 = string.ascii_uppercase
+s3 = string.digits
+s4 = string.punctuation
+
+plen = int(input("Enter your password length : \n"))
+s = []
+s.extend(list(s1))
+s.extend(list(s2))
+s.extend(list(s3))
+s.extend(list(s4))
+
+random.shuffle(s)
+
+print("Your new Password is : ")
+print("".join(s[0:plen]))
